e73c2551f36b4eeb4c8c0c5516e6604012e4d189
KyNag <cynosureky@gmail.com>
2014-03-18 20:04:50 +0400

    Pushfrom workstation1

---
+++ AllXmlParser/src/kay/xml/parsersample/FeedParser.java
@@ -0,0 +1,6 @@
+package kay.xml.parsersample;
+import java.util.List;
+
+public interface FeedParser {
+	List<Message> parse();
+}
---
+++ AllXmlParser/src/kay/xml/parsersample/ParserType.java
@@ -0,0 +1,8 @@
+package kay.xml.parsersample;
+/**
+ * 
+ */
+
+public enum ParserType{
+	SAX, DOM, ANDROID_SAX, XML_PULL;
+}
